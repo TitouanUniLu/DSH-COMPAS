300c087ff9ad2c91c4579456f46690aebb32521a
issaaaaaaaaam <is.jomaa@gmail.com>
2023-04-19 18:16:04 +0200

    script bar charts upgraded

--- dsh-week1.py
+++ dsh-week1.py
@@ -4,18 +4,28 @@ import matplotlib.pyplot as plt
 df = pd.read_csv('compas-scores-raw.csv')
 
 for column in df.columns:
-    # Count the occurrences of unique values in the column
-    value_counts = df[column].value_counts()
+    # Check if the column contains strings
+    if df[column].dtype == 'object':
+        # Count the occurrences of unique strings in the column
+        value_counts = df[column].value_counts()
+        
+        # Plot a bar chart of the frequency
+        plt.bar(value_counts.index, value_counts.values)
+        plt.xlabel(column)
+        plt.ylabel('Frequency')
+        plt.title(f'Bar Chart of {column}')
+        plt.savefig(f'Bar Chart of {column}')
 
-    # Plot a histogram of the frequency
-    plt.bar(value_counts.index, value_counts.values)
-    plt.xlabel(column)
-    plt.ylabel('Frequency')
-    plt.title(f'Histogram of {column}')
-    plt.show()
-
-  
-
+    else:
+        # Count the occurrences of unique values in the column
+        value_counts = df[column].value_counts()
+        
+        # Plot a histogram of the frequency
+        plt.hist(df[column], bins=10)
+        plt.xlabel(column)
+        plt.ylabel('Frequency')
+        plt.title(f'Bar Chart of {column}')
+        plt.savefig(f'Bar Chart of {column}')
 
 
 
