6c8aa2030a73c39c544f527e2dfa21debedb6d80
Bill (Vasileios) Nikolopoulos <vnikolo@outlook.com>
2023-04-26 13:58:22 +0200

    Continuous Features
    Dealt with the Continuous Features and created a new version of the .cvs

--- dsh-week2.py
+++ dsh-week2.py
@@ -1,8 +1,9 @@
 import pandas as pd
 import matplotlib.pyplot as plt 
-from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import MinMaxScaler
 
 df = pd.read_csv('compas-scores-re_col.csv')
+
 
 
 ### Use the below to drop the irrelevant columns of the csv reduced file(2000 lines).
@@ -19,7 +20,8 @@ df = pd.read_csv('compas-scores-re_col.csv')
 # print('First 5 rows of the updated CSV file
 
 
-# ## One-Hot Encoding 
+
+### One-Hot Encoding 
 # # Define the columns for which to perform one-hot encoding
 # cols_to_encode = ['Agency_Text', 'Sex_Code_Text', 'Ethnic_Code_Text', "Language", "LegalStatus", 
 #                     "CustodyStatus", "MaritalStatus"]
@@ -33,4 +35,22 @@ df = pd.read_csv('compas-scores-re_col.csv')
 
 # # Export the updated DataFrame to a new CSV file
 # df_encoded.to_csv('compas-scores-encoding.csv', index=False)
+
+
+
+### Deal with continuous features and round them up to 4 digits after decimal point
+# Load the dataset into a pandas DataFrame
+# df_cf = pd.read_csv('compas-scores-encoding.csv')
+
+# # Define the columns to scale
+# cols_to_scale = ['RawScore']
+
+# # Perform min-max scaling for the selected columns
+# scaler = MinMaxScaler()
+# df_cf[cols_to_scale] = scaler.fit_transform(df_cf[cols_to_scale])
+
+# # Round all values in a specific column to 4 digits after the decimal point
+# df_cf['RawScore'] = df_cf['RawScore'].round(4)
+
+# df_cf.to_csv('compas-scores-cf.csv', index=False)
 
