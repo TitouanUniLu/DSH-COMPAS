87b57ee912e3c30b2d651ad9f562411c40736840
Bill (Vasileios) Nikolopoulos <vnikolo@outlook.com>
2023-05-10 22:07:48 +0200

    Week3_F

--- dsh-week3.py
+++ dsh-week3.py
@@ -214,40 +214,181 @@ from sklearn.preprocessing import MinMaxScaler
 # # show plot
 # plt.show()
 
-# load data from csv file
-data = pd.read_csv('Old_Data_Sets/compas-scores-reduced.csv')
+# # load data from csv file
+# data = pd.read_csv('Old_Data_Sets/compas-scores-reduced.csv')
 
-# Keep only the rows where Scale_ID equals 8
-data = data[data['Scale_ID'] == 8]
+# # Keep only the rows where Scale_ID equals 8
+# data = data[data['Scale_ID'] == 8]
 
-# select relevant columns
-risk_and_marital_status = data[['RecSupervisionLevelText', 'MaritalStatus']]
+# # select relevant columns
+# risk_and_marital_status = data[['RecSupervisionLevelText', 'MaritalStatus']]
 
-# group data by risk level and marital status
-grouped_data = risk_and_marital_status.groupby(['RecSupervisionLevelText', 'MaritalStatus']).size().reset_index(name='count')
+# # group data by risk level and marital status
+# grouped_data = risk_and_marital_status.groupby(['RecSupervisionLevelText', 'MaritalStatus']).size().reset_index(name='count')
+
+# # pivot data to create stacked bar chart
+# pivot_data = grouped_data.pivot(index='RecSupervisionLevelText', columns='MaritalStatus', values='count')
+
+# # define a color scheme for each marital status category
+# colors = {'Single': 'blue', 'Married': 'green', 'Divorced': 'purple', 'Separated': 'red', 'Widowed': 'gray', "Significant Other": 'yellow'}
+
+# pivot_data = pivot_data[colors.keys()]
+
+# # create stacked bar chart
+# ax = pivot_data.plot(kind='bar', stacked=True, color=[colors[key] for key in pivot_data.columns], figsize=(12,6))
+
+# # set labels and title
+# ax.set_xlabel('Group Risk Level')
+# ax.set_ylabel('Number of Cases')
+# ax.set_title('Distribution of Marital Status by Group Risk Level')
+
+# # set legend with custom colors
+# handles, labels = ax.get_legend_handles_labels()
+# ax.legend(handles, colors.keys(), loc='upper right')
+
+# # show plot
+# plt.show()
+
+# # load data from csv file
+# data = pd.read_csv('Old_Data_Sets/compas-scores-reduced.csv')
+
+# # select relevant columns
+# race_and_scores = data[['Ethnic_Code_Text', 'ScoreText']]
+
+# # group data by race and COMPAS score categories
+# grouped_data = race_and_scores.groupby(['Ethnic_Code_Text', 'ScoreText']).size().reset_index(name='count')
+
+# # pivot data to create stacked bar chart
+# pivot_data = grouped_data.pivot(index='Ethnic_Code_Text', columns='ScoreText', values='count')
+
+# # define a color scheme for each COMPAS score category
+# colors = {'Low': 'green', 'Medium': 'orange', 'High': 'red'}
+
+# # reorder columns to match order in dictionary
+# pivot_data = pivot_data[colors.keys()]
+
+# # create stacked bar chart
+# ax = pivot_data.plot(kind='bar', stacked=True, color=[colors[key] for key in pivot_data.columns], figsize=(12,6))
+
+# # set labels and title
+# ax.set_xlabel('Ethnic_Code_Text')
+# ax.set_ylabel('Number of Cases')
+# ax.set_title('Distribution of COMPAS Score Categories by Race')
+
+# # set legend with custom colors
+# handles, labels = ax.get_legend_handles_labels()
+# ax.legend(handles, colors.keys(), loc='upper right')
+
+# # show plot
+# plt.show()
+
+
+# # Load data from CSV file and select relevant columns
+# data = pd.read_csv('Old_Data_Sets/compas-scores-reduced.csv')
+# data = data[['Sex_Code_Text', 'DecileScore']]
+
+# # Group data by gender and COMPAS score category
+# grouped_data = data.groupby(['Sex_Code_Text', 'DecileScore']).size().reset_index(name='count')
+
+# # Calculate proportion of individuals in each group
+# total_counts = grouped_data.groupby(['Sex_Code_Text'])['count'].transform('sum')
+# grouped_data['proportion'] = grouped_data['count'] / total_counts
+
+# # Pivot data to create stacked bar chart
+# pivot_data = grouped_data.pivot(index='Sex_Code_Text', columns='DecileScore', values='proportion')
+
+# # Define color scheme for each COMPAS score category
+# colors = {1: 'green', 2: 'blue', 3: 'red', 4: 'orange', 5: 'purple', 6: 'gray', 7: 'pink', 8: 'brown', 9: 'yellow', 10: 'black'}
+
+# # reorder columns to match order in dictionary
+# pivot_data = pivot_data[colors.keys()]
+
+# # Create stacked bar chart
+# ax = pivot_data.plot(kind='bar', stacked=True, color=[colors[col] for col in pivot_data.columns], figsize=(12,6))
+
+# # Set labels and title
+# ax.set_xlabel('Sex_Code_Text')
+# ax.set_ylabel('Proportion')
+# ax.set_title('Proportion of Males and Females by COMPAS Score Category')
+
+# # Set legend with custom colors
+# handles, labels = ax.get_legend_handles_labels()
+# ax.legend(handles, labels, loc='upper right')
+
+# # Show plot
+# plt.show()
+
+# # Load data from csv file
+# data = pd.read_csv('Old_Data_Sets/compas-scores-raw.csv')
+
+# # Keep only the rows where "is_recid" equals 1 or -1
+# data = data[(data['is_recid'] == 1) | (data['is_recid'] == -1)]
+
+# # Select relevant columns
+# age_and_recid = data[['age', 'is_recid']]
+
+# # Create scatter plot
+# sns.scatterplot(data=age_and_recid, x='age', y='is_recid')
+
+# # Set labels and title
+# plt.xlabel('Age')
+# plt.ylabel('Likelihood of Recidivism')
+# plt.title('Relationship Between Age and Likelihood of Recidivism')
+
+# # Show plot
+# plt.show()
+
+# ### Relationship Between Age and Likelihood of Recidivism
+# # load data from csv file
+# data = pd.read_csv('compas-scores-recidivism.csv')
+
+# # select relevant columns
+# age_and_score = data[['DateOfBirth', 'DecileScore']]
+
+# # create scatter plot
+# plt.scatter(age_and_score['DateOfBirth'], age_and_score['DecileScore'], alpha=0.5)
+
+# # set labels and title
+# plt.xlabel('DateOfBirth')
+# plt.ylabel('COMPAS Recidicism Decile Score')
+# plt.title('Relationship Between Age and Likelihood of Recidivism')
+
+# # show plot
+# plt.show()
+
+data = pd.read_csv('compas-scores-recidivism.csv')
+
+# categorize individuals into different age groups
+age_bins = [0, 20, 35, 45, 60, 75, 100]
+age_labels = ['<20', '20-35', '36-45', '46-60', '61-75', '>75']
+data['Age_Group'] = pd.cut(data['DateOfBirth'].astype(str).apply(lambda x: 123 - int(x[-2:])), bins=age_bins, labels=age_labels)
+
+# filter relevant columns
+compas_age = data[['Age_Group', 'ScoreText']]
+
+# group data by age group and COMPAS score category
+grouped_data = compas_age.groupby(['Age_Group', 'ScoreText']).size().reset_index(name='count')
 
 # pivot data to create stacked bar chart
-pivot_data = grouped_data.pivot(index='RecSupervisionLevelText', columns='MaritalStatus', values='count')
+pivot_data = grouped_data.pivot(index='Age_Group', columns='ScoreText', values='count')
 
-# define a color scheme for each marital status category
-colors = {'Single': 'blue', 'Married': 'green', 'Divorced': 'purple', 'Separated': 'red', 'Widowed': 'gray', "Significant Other": 'yellow'}
+# set colors for each COMPAS score category
+colors = {'Low': 'green', 'Medium': 'yellow', 'High': 'red'}
+
+# reorder columns to match order in dictionary
+pivot_data = pivot_data[colors.keys()]
 
 # create stacked bar chart
 ax = pivot_data.plot(kind='bar', stacked=True, color=[colors[key] for key in pivot_data.columns], figsize=(12,6))
 
 # set labels and title
-ax.set_xlabel('Recidivism Risk Level')
+ax.set_xlabel('Age Group')
 ax.set_ylabel('Number of Cases')
-ax.set_title('Distribution of Marital Status by Recidivism Risk Level')
+ax.set_title('Proportion of Individuals in Different Age Categories by COMPAS Score Category')
 
 # set legend with custom colors
 handles, labels = ax.get_legend_handles_labels()
-ax.legend(handles, colors.keys(), loc='upper right')
+ax.legend(handles, labels, loc='upper right')
 
 # show plot
 plt.show()
-
-
-
-
-
